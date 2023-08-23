77ba16543fb2842883710c737b366c4cfcc0dddb
Olia Rosiak <olia.rosiak@gmail.com>
2023-08-23 23:52:13 +0300

    upd routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,39 @@
 const express = require("express");
 
 const {
-  getAll,
-  getById,
-  add,
-  deleteById,
-  updateById,
+  getAllContacts,
+  getContactById,
+  addNewContact,
+  deleteContactById,
+  updateContactById,
+  updateStatusContact,
 } = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
-const { addSchema } = require("../../schemas/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { addSchema, favoriteSchema } = require("../../schemas/contacts");
 
 const router = express.Router();
 
-router.get("/", getAll);
+router.get("/", getAllContacts);
 
-router.get("/:contactId", getById);
+router.get("/:contactId", isValidId, getContactById);
 
-router.post("/", validateBody(addSchema, "Missing required name field"), add);
+router.post("/", validateBody(addSchema), addNewContact);
 
-router.delete("/:contactId", deleteById);
+router.delete("/:contactId", isValidId, deleteContactById);
 
 router.put(
   "/:contactId",
-  validateBody(addSchema, "Missing fields"),
-  updateById
+  isValidId,
+  validateBody(addSchema),
+  updateContactById
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(favoriteSchema),
+  updateStatusContact
 );
 
 module.exports = router;
