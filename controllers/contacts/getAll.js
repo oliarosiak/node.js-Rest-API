78af9eb5effc37a386db5584178569262d610449
Olia Rosiak <olia.rosiak@gmail.com>
2023-08-30 22:20:52 +0300

    upd controllers/contacts

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,10 +4,21 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate(
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
     "owner",
     "email subscription"
   );
+
+  if (favorite === "true") {
+    const favoriteContacts = result.filter(
+      (contact) => contact.favorite === true
+    );
+
+    res.json(favoriteContacts);
+  }
+
   res.json(result);
 };
 
