cf954be375531b2702d61cd5e7a4bb5022fdb238
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-05 20:44:30 +0300

    create middleware - upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const { validateBody } = require("./validateBody");
 const { isValidId } = require("./isValidId");
 const { isValidToken } = require("./isValidToken");
+const { upload } = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   isValidToken,
+  upload,
 };
