31d5d0f5dad974c926b1f13dadc345cd187461a6
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-05 23:14:00 +0300

    add controller / avatar.js

---
+++ controllers/auth/avatar.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+
+const { User } = require("../../models/user");
+const { ctrlWrapper } = require("../../helpers");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const avatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  console.log(tempUpload);
+  console.log(resultUpload);
+
+  Jimp.read(tempUpload)
+    .then((image) => {
+      return image.resize(250, 250).write(resultUpload);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  await fs.rename(tempUpload, resultUpload);
+
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
+module.exports = {
+  avatar: ctrlWrapper(avatar),
+};
