4676c3d55d8c4529337a4ed2326685d390435b5e
Olia Rosiak <olia.rosiak@gmail.com>
2023-09-11 18:09:19 +0300

    upd controllers (register + login)

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+require("dotenv").config();
 
+const { BASE_URL } = process.env;
 const { User } = require("../../models/user");
-const { HttpError, ctrlWrapper } = require("../../helpers");
+const { HttpError, ctrlWrapper, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,12 +17,22 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid(20);
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const veryfyEmail = {
+    to: email,
+    subject: "Verify your email",
+    html: `<a target='_blank' href='${BASE_URL}/api/auth/verify/${verificationToken}'>Clich here to verify your email</a>`,
+  };
+
+  await sendEmail(veryfyEmail);
 
   res.status(201).json({
     user: {
